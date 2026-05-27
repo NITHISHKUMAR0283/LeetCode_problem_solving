cbc3c5b7647493314e01428683cad5f9d4ab5935
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-05-27 20:10:04 +0530

    Time: 2 ms (83.87%), Space: 10.2 MB (31.94%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,62 +1,47 @@
 class Solution {
 public:
-    vector<int> kmp(string a ,string b){
-        int m = b.length();
+    int base = 1000000;
+    int rabin_karp(string text , string pattern){
+        if(text == "" or pattern=="")return -1;
+        int n = text.length();
+        int m = pattern.length();
 
-        vector<int> lcs(m,0);
-        
-        int i=1;
-        int len =0;
-
-        while(i<m){
-            if(i<m && b[i]==b[len]){
-                len++;
-                lcs[i]=len;
-                i++;
-            }
-            else{
-                if(len!=0){
-                    len = lcs[len-1];
-                }
-                else {
-                    lcs[i]=0;
-                    i++;
+        int power = 1;
+        for(int i = 0;i<m;i++){
+            power=(power*31)%base;
+        }
+        int hash = 0;
+        for(int i=0;i<m;i++){
+            hash = (hash*31+pattern[i])%base;
+        }
+        int l = 0;
+        int curr_hash =0;
+        for(int r = 0;r<n;r++){            
+            curr_hash = (curr_hash*31+text[r])%base;
+            if(r-l+1>m){
+                curr_hash = ((curr_hash-text[l]*power)%base+base)%base;
+                l++;
+            }           
+            int length = r-l+1;
+            if(length<m)continue;
+            if(curr_hash == hash){
+                if(text.substr(l,m)==pattern){
+                    return l;
                 }
             }
         }
-        return lcs;
-    }
-    bool substring_kmp(string a , string b){
-        int n = a.length();
-        int m = b.length();
-
-        vector<int> lcs = kmp(a,b);
-        int i =0;
-        int j = 0;
-        while(i<n){
-            if(a[i]==b[j]){
-                i++;
-                j++;
-            }
-            if(j==m)return true;
-            else if(i<n && a[i]!=b[j]){
-                if(j==0)i++;
-                else j=lcs[j-1];
-            }
-        }
-        return false;
+        return -1;
     }
     int repeatedStringMatch(string a, string b) {
-        int n =a.length();
         int m = b.length();
         string temp = "";
         int count = 0;
-        while(temp.size()<m){
-            temp+=a;
-            count ++;
+        while(temp.length()<m){
+            temp += a;
+            count++;
         }
-        if(substring_kmp(temp,b))return count;
-        else if (substring_kmp(temp+a,b))return count+1;
+        if(rabin_karp(temp,b)!=-1)return count;
+        if(rabin_karp(temp+a,b)!=-1)return count+1;
         return -1;
     }
 };
