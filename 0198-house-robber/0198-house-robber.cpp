1ef06b6980fa8962f6125eb11260fd9ea65502ab
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-06-01 06:37:48 +0530

    Time: 0 ms (100%), Space: 10.7 MB (20.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,11 +6,10 @@ public:
         if(n==1)return nums[0];
         if(n==2)return max(nums[0],nums[1]);
         dp[0]=nums[0];
-        dp[1]=nums[1];
-        dp[2]=nums[2]+dp[0];
-        for(int i =3;i<n;i++){
-            dp[i]=max(dp[i-2],dp[i-3])+nums[i];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i =2;i<n;i++){
+            dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
         }
-        return max(dp[n-1],dp[n-2]);
+        return dp[n-1];
     }
 };
