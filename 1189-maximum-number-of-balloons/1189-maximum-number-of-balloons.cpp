7b69c4b41b6aefe2a44b3702b76998a0941b3622
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-06-22 22:16:10 +0530

    Time: 0 ms (100%), Space: 9.2 MB (15.37%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -5,13 +5,12 @@ public:
         for(auto c : text){
             freq[c]++;
         }
-        if(freq.contains('l')) freq['l']=freq['l']/2;
-        if(freq.contains('o'))freq['o']=freq['o']/2;
+        freq['l']=freq['l']/2;
+        freq['o']=freq['o']/2;
         string word = "balon";
         int maxi = INT_MAX;
         
         for(auto c : word){
-            if(!freq.count(c))return 0;
             maxi = min(maxi,freq[c]);
         }
         return maxi;
