73621ca01871a5b3856a0c0b68345de925caf7a8
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-06-24 21:52:03 +0530

    Time: 6 ms (75.42%), Space: 91.6 MB (60.17%) - LeetHub

---
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minIncrementOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long > dp(n,0);
+        dp[0]=max(0,k-nums[0]);
+        dp[1]=max(0,k-nums[1]);
+        dp[2]=max(0,k-nums[2]);
+
+        for(int i = 3;i<n;i++){
+            int cost = max(0,k-nums[i]);
+            dp[i]=cost + min(dp[i-3],min(dp[i-1],dp[i-2]));
+        }
+        long long  ans = min(dp[n-1],min(dp[n-2],dp[n-3]));
+        return ans;
+    }
+};
