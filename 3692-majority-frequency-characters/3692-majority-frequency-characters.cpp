2ce34d0de5b29343f89fad09fbb254864a6329b9
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-07-11 20:56:29 +0530

    Time: 7 ms (25.13%), Space: 10.8 MB (16.67%) - LeetHub

--- 3692-majority-frequency-characters/3692-majority-frequency-characters.cpp
+++ 3692-majority-frequency-characters/3692-majority-frequency-characters.cpp
@@ -8,16 +8,19 @@ public:
         unordered_map<int,vector<char>> bucket ;
         vector<char> ans = {s[0]};
         int max_len = 0;
+        int ans_f = 0;
         for(auto& [c,f] : freq){
             bucket[f].push_back(c);
             
-            if (max_len<bucket[f].size() || (max_len==bucket[f].size() && freq[ans[0]]<freq[bucket[f][0]])){
-                ans = bucket[f];
+            if (max_len<bucket[f].size() || (max_len==bucket[f].size() && ans_f<f)){
+                
                 max_len = bucket[f].size();
+                ans_f = f;
             }
 
 
         }
+        ans = bucket[ans_f];
         string res (ans.begin(),ans.end());
         return res;
     }
