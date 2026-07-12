5827d3e4f2ca200a91a5f000587506b6ab0155b4
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-07-12 20:44:39 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void explore(queue<int>& q ,unordered_set<int>& visited , vector<vector<int>> & isConnected){
+    void explore(queue<int>& q ,vector<bool> & visited , vector<vector<int>> & isConnected){
         
         int n = isConnected.size();
         while(!q.empty()){
@@ -8,22 +8,23 @@ public:
             q.pop();
             for(int i = 0;i<n;i++){
                 if(isConnected[current][i]==1){
-                    if(visited.find(i)!=visited.end())continue;
-                    visited.insert(i);
+                    if(visited[i])continue;
+                    visited[i]=true;;
                     q.push(i);
                 }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        unordered_set<int> visited;
+        
+        int n = isConnected.size();
+        vector<bool> visited(n,false);
 
         queue<int> q ;
-        int n = isConnected.size();
         int no_prov = 0;
         for(int i = 0;i<n;i++){
-            if(visited.find(i)!=visited.end())continue;
-            visited.insert(i);
+            if(visited[i])continue;
+            visited[i];
             q.push(i);
             no_prov++;
             explore(q,visited,isConnected);
