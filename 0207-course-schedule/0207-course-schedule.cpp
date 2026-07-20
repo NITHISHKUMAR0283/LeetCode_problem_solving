d4711ee5143a0e776bb45a30bbf0bfdc01ebd818
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-07-20 19:38:51 +0530

    Time: 72 ms (5.01%), Space: 132.8 MB (5.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
-        unordered_map<int,vector<int>> adj;        
-        vector<int> indegree(numCourses);
+        vector<vector<int>> adj(numCourses,vector<int>(numCourses,0));        
+        vector<int> indegree(numCourses,0);
 
         for(auto& pair:prerequisites){
-                adj[pair[1]].push_back(pair[0]);                
+                adj[pair[1]][pair[0]]=1;              
                 indegree[pair[0]]++;
             }
         
-
-
         queue <int> q;
 
         for(int u = 0;u<numCourses;u++){
@@ -24,7 +22,8 @@ public:
         while(!q.empty()){
             auto node = q.front();
             q.pop();
-            for(auto v :adj[node]){
+            for(int v =0; v<numCourses;v++){
+                if(adj[node][v]!=1)continue;
                 indegree[v]--;
                 if(indegree[v]==0){
                     q.push(v);
