ed5ea6bde389802e876def467d0ab58737d2502d
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-07-20 19:41:45 +0530

    Time: 4 ms (63.67%), Space: 19.2 MB (81.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
-        vector<vector<int>> adj(numCourses,vector<int>(numCourses,0));        
+        vector<vector<int>> adj(numCourses);        
         vector<int> indegree(numCourses,0);
 
         for(auto& pair:prerequisites){
-                adj[pair[1]][pair[0]]=1;              
+                adj[pair[1]].push_back(pair[0]);;              
                 indegree[pair[0]]++;
             }
         
         queue <int> q;
-
+        int count = 0;
         for(int u = 0;u<numCourses;u++){
             if(indegree[u]==0){
                 q.push(u);
@@ -22,17 +22,15 @@ public:
         while(!q.empty()){
             auto node = q.front();
             q.pop();
-            for(int v =0; v<numCourses;v++){
-                if(adj[node][v]!=1)continue;
+            count++;
+            for(auto v : adj[node]){
                 indegree[v]--;
                 if(indegree[v]==0){
                     q.push(v);
                 }
             }
         }
-        for(auto degree:indegree){
-            if(degree!=0)return false;
-        }
+        if(count!=numCourses)return false;
         return true;
     }
 };
