459360be307df88d6d3278e8d88e2ececf51a19c
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-08-04 05:53:46 +0530

    Time: 45 ms (81.51%), Space: 66.3 MB (88.43%) - LeetHub

---
+++ 4013-count-subarrays-with-even-odd-ratio-ii/4013-count-subarrays-with-even-odd-ratio-ii.cpp
@@ -0,0 +1,62 @@
+class fenwick{
+    private:
+    vector<int>bit;
+    int n;
+    public:
+    fenwick(int m){
+        bit.assign(m+1,0);
+        n=m;
+    }
+    void add(int value,int index){
+
+        while(index<=n){
+            bit[index]+=value;
+            index += index & (-index);
+
+        }
+    }
+    int query (int index){
+        int count = 0;
+        while(index>0){
+            count+=bit[index];
+            index -= index&(-index);
+        }
+        return count;
+    }
+
+};
+class Solution {
+public:
+    long long countRatioSubarrays(vector<int>& nums, int a, int b) {
+        int n = nums.size();
+        vector<long long > prefix(n+1,0);
+        long long sum = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i]%2==0)sum+=b;
+            else sum-=a;
+            prefix[i+1]=sum;
+        }
+
+        vector<long long >  comp(prefix.begin(),prefix.end());
+        sort(comp.begin(),comp.end());
+
+        comp.erase(unique(comp.begin(),comp.end()),comp.end());
+        fenwick fn(comp.size());
+        
+        long long ans=0;
+        for(int i = 0;i<=n;i++){
+            int rank = lower_bound(comp.begin(),comp.end(),prefix[i])-comp.begin()+1;
+            
+
+            int smaller = fn.query(rank-1);
+
+            ans += i-smaller;
+            
+            fn.add(1,rank);
+
+        }
+        return ans;
+
+
+    }
+};
