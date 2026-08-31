c2303d1e8db8309031623652955b7d5f88640c5a
NITHISHKUMAR A <nk0283@srmist.edu.in>
2026-08-31 06:18:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    bool isSubsetSum(vector<int>& arr, int sum) {
-        // code here
-        
-        vector<bool> present (sum+1,false);
-        int n = arr.size();
-        
-        present[0]=true;
-        for(int i = 0;i<n;i++){
-            int curr = arr[i];
-            //assuming using the current as the last step can we reach the sum of prev
-            for(int res = sum;res>-1;res--){
-                int prev = res-curr;
-                if(prev<0)continue;
-                if(present[prev]==true){
-                    present[res]=true;
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum  = accumulate(nums.begin(),nums.end(),0);
+
+        if(sum%2==1)return false;
+
+        int target = sum/2;
+
+        vector<vector<bool>> dp (n+1,vector<bool> (target+1,false));
+
+        for( int item = 0 ; item <=n ; item ++){
+            dp[item][0]=true;
+        }
+
+        for (int item = 1; item <= n ; item++ ){
+            for( int sum = 1; sum <= target ; sum++ ){
+
+                int need = sum - nums[item-1];
+                bool possi = false;
+                if(need >=0 && dp[item-1] [need]==true){
+                    possi = true; 
                 }
+                dp[item] [ sum ] = dp[item-1][sum] || possi;
             }
         }
-        return present[sum];
+        for( int item = 0 ; item <= n ; item ++ ){
+            for( int sum = 0 ; sum <= target ; sum ++ ){
+                cout<< dp[item][sum]<< " ";
+            }
+        }
+        return dp[n][target];
     }
-    bool canPartition(vector<int>& nums) {
-        int sum = accumulate (nums.begin(),nums.end(),0);
-        if(sum%2==1)return false;
-        int target = sum/2;
-        return isSubsetSum(nums,target);
-
-
-
-    }
-
 };
